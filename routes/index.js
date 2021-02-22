9720f3e302201c542091447f7465b946226511eb
kimjinss <77815816+kimjinss@users.noreply.github.com>
2021-02-22 14:32:20 +0900

    test17

--- routes/index.js
+++ routes/index.js
@@ -4,43 +4,41 @@ const router = express.Router();
 const app = express();
 const Query = require('pg').Query
 
+const client = new Client({
+    user: 'test1',
+    host: '127.0.0.1',
+    // host: '59.3.55.93',
+    database: 'test1db',
+    password: 'test1',
+    port: '5432',
+    max: 10, // max number of clients in the pool
 
+    idleTimeoutMillis: 30000, // how long a client
 
-// const client = new Client({
-//     user: 'test1',
-//     // host: '127.0.0.1',
-//     host: '59.3.55.93',
-//     database: 'test1db',
-//     password: 'test1',
-//     port: '5432',
-//     max: 10, // max number of clients in the pool
-//
-//     idleTimeoutMillis: 30000, // how long a client
-//
-// });
+});
 
-
-// client.connect();
-//
-// client.query('SELECT NOW()', (err, res) => {
-//     console.log(err, res)
-//     client.end()
-// });
-
-// client.connect(err => {
-//     if (err) {
-//         console.error('connection error', err.stack)
-//     } else {
-//         console.log('success!')
-//     }
-// });
-
+client.connect(err => {
+    if (err) {
+        console.error('connection error', err.stack)
+    } else {
+        console.log('success!')
+    }
+});
 
 
 
 router.post('/post', (req, res) => {
     console.log('who get in here post /users');
+    const query = new Query("SELECT * FROM user;")
+    client.query(query)
     var inputData;
+
+    const rows = []; /** * row에서 데이터 가져오고 end에서 검색할 때 발생한 각종 정보, error는 오류 발생시 */
+
+    query.on("row",row=>{
+        rows.push(row);
+    });
+
 
     req.on('data', (data) => {
         inputData = JSON.parse(data);
@@ -50,8 +48,15 @@ router.post('/post', (req, res) => {
         console.log("user_id : "+inputData.user_id + " , name : "+inputData.name);
     });
 
-    res.write("OK!");
-    res.end();
+    query.on('end', () => {
+        console.log(rows[0]);
+        console.log('query done')
+        res.send(rows[0].user);
+        res.status(200).end();
+    });
+
+    // res.write("OK!");
+    // res.end();
 });
 
 
